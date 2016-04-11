299dcf823e5a8dc9252317ce9ea4e1846c073249
brunon80 <bruno_rocha2008@hotmail.com>
2016-04-11 17:44:16 -0300

    metodo de autenticacao no servidor

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var app    = express();
+var jwt    = require('jsonwebtoken');
 var models = require('../models/index');
+var app    = express();
+var config = require('../../config');
+var session = require('client-sessions');
+var mySession;
+
+app.set('superSecret', config.secret);
 
 router.get('/', function(req, res, next) {
   res.render('postit', { title: 'Express' });
@@ -21,6 +29,40 @@ router.get('/users', function(req, res) {
   });
 });
 
+router.post('/authenticate', function(req, res) {
+  models.User.findOne({
+    where: {
+      email: req.body.email
+    }
+    }).then(function(user) {
+        
+        if (!user) {
+          res.json({ success: false, message: 'Authentication failed. User not found.' });
+
+        }else if (user) {
+
+        // check if password matches
+        if (user.password != req.body.password) {
+          res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+      }else {
+
+          
+          // if user is found and password is right
+          // create a token
+          var token = jwt.sign(user.email,app.get('superSecret'));
+
+          res.json({
+            email: user.email,
+            success: true,
+            message: 'Enjoy your token!',
+            token: token,
+            teste: mySession
+          });
+        }
+       } 
+      })
+  });
+
 router.delete('/user/:id', function(req, res) {
   models.User.destroy({
     where: {
@@ -34,6 +76,7 @@ router.delete('/user/:id', function(req, res) {
 router.get('/postits', function(req, res) {
   models.Postitdbp.findAll({}).then(function(postits) {
     res.json(postits);
+
   });
 });
 
@@ -89,5 +132,45 @@ router.delete('/postit/:id', function(req, res) {
     res.json(postit);
   });
 });
+
+router.use(function(req, res, next) {
+
+  // check header or url parameters or post parameters for token
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+  // decode token
+  if (token) {
+
+    // verifies secret and checks exp
+    jwt.verify(token, app.get('superSecret'), function(err, decoded) {      
+      if (err) {
+        return res.json({ success: false, message: 'Failed to authenticate token.' });    
+      } else {
+        // if everything is good, save to request for use in other routes
+        req.decoded = decoded;    
+        next();
+      }
+    });
+
+  } else {
+
+    // if there is no token
+    // return an error
+    return res.status(403).send({ 
+        success: false, 
+        message: 'No token provided.' 
+    });
+    
+  }
+});
+
+// apply the routes to our application with the prefix /api
+app.use('/authenticate', router);
+
+// route to show a random message 
+
+
+
+// route to return all users 
 
 module.exports = router;
